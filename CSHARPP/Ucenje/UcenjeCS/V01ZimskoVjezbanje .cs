e9e3f6a1c86be2594e3971b02f6695a26017cc25
Martina Petak <petak.martina@gmail.com>
2024-01-07 20:10:38 +0100

    vježbe

--- CSHARPP/Ucenje/UcenjeCS/V01ZimskoVjezbanje .cs
+++ CSHARPP/Ucenje/UcenjeCS/V01ZimskoVjezbanje .cs
@@ -4,27 +4,42 @@
     {
         public static void Izvedi()
         {
-            //Napisati program koji ispisuje sve brojeve od 1 do 100
+            Console.WriteLine("Napisati program koji ispisuje sve brojeve od 1 do 100");
 
-            //for (int i=0; i < 100; i++)
-            //{
-            //    Console.WriteLine(i+1);
-            //}
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(i + 1);
+            }
+            Console.WriteLine("Napisati program koji ispisuje sve brojeve od 100 do 1");
 
-            //Napisati program koji ispisuje sve brojeve od 100 do 1
+            for (int i = 101; i > 1; i--)
+            {
+                Console.WriteLine(i - 1);
+            }
+            Console.WriteLine("Napisati program koji ispisuje sve brojeve od 1 do 100 koji su cjelobrojno djeljivi s 7");
 
-            //    for (int i = 101; i >1; i--)
-            //    {
-            //        Console.WriteLine(i - 1);
-            //    }
-            //
-            //Napisati program koji ispisuje sve brojeve od 1 do 100 koji su cjelobrojno djeljivi s 7
+            for (int i = 7; i < 100; i += 7)
 
-            for (int i = 7; i <100; i+=7) 
-
-            Console.WriteLine(i);
-
-        }
+                Console.WriteLine(i);
         
+            Console.WriteLine("4. Napisati program koji unosi brojeve sve dok ne unese broj veći od 100, a zatim ispisuje koliko je bilo pokušaja unosa\r\n");
+
+            int BrojUnosa = 0;
+            for (; ;)
+            {
+                Console.WriteLine("Unesite broj veci od 100: ");
+                int UneseniBroj = int.Parse(Console.ReadLine());
+                if (UneseniBroj <= 100)
+                {
+                    BrojUnosa++;
+                }
+                else
+                {
+                    Console.WriteLine($"Unijeli ste broj manji od 100 {BrojUnosa} puta.");
+                   
+                }
+                break;
+            }
+        }
     }
 }
---
+++ CSHARPP/Ucenje/WebAPI/Controllers/V02ZimskoVjezbanje.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("V02")]
+    public class V02ZimskoVjezbanje : ControllerBase
+    {
+        [HttpGet]
+        [Route("zad1")]
+        //Ruta ne prima niti jedan parametar i vraća zbroj prvih 100 brojeva
+        public int Zad1()
+        {
+            int Zbroj = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                Zbroj += i;
+            }
+
+            return Zbroj;
+        }
+
+
+        //Ruta ne prima niti jedan parametar i vraća niz s svim parnim brojevima od 1 do 57
+        [HttpGet]
+        [Route("zad2")]
+
+        public int[] zad2()
+        {
+            int[] niz = new int[57 / 2];
+            int n = 0;
+
+            for (int i = 1; i < 57; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    niz[n++] = i;
+                }
+            }
+            return niz;
+        }
+
+        //Ruta ne prima niti jedan parametar i vraća zbroj svih parnih brojeva od 2 do 18
+        [HttpGet]
+        [Route("Zad3")]
+
+        public int Zad3()
+        {
+            int suma = 0;
+            for (int i = 2; i <= 18; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    suma += i;
+                }
+            }
+
+            return suma;
+        }
+
+        //Ruta prima jedan parametar koji je cijli broj i vraća zbroj svih brojava od 1 do primljenog broja
+
+        [HttpGet]
+        [Route("Zad4")]
+
+        public int Zad4(int broj)
+        {
+            int suma = 0;
+
+            for (int i = 0; i <= broj; i++)
+            {
+                suma += i;
+            }
+            return suma;
+        }
+    }
+}
