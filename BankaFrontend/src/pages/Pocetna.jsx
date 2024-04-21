aed902551e3294ab776b8f8f16fcef8a3db83ca3
Martina Petak <petak.martina@gmail.com>
2024-04-21 18:12:25 +0200

    slika dodana

--- BankaFrontend/src/pages/Pocetna.jsx
+++ BankaFrontend/src/pages/Pocetna.jsx
@@ -2,12 +2,20 @@ import { Container } from "react-bootstrap";
 
 export default function Pocetna ()
 {
+    
 
     return(
         <>
         <Container>
+            
             Dobrodošli na Banka APP
+
+
         </Container>
+
+        <img slika src="mcduck.gif" alt="" />
+
+
         </>
     );
 
